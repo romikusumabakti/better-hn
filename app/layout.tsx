1364ced90bf4f9cd2d98326988b06bd675b9b65f
Romi Kusuma Bakti <romikusumab@gmail.com>
2026-05-24 14:33:22 +0700

    Change default theme from dark to system

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
 			<body className="antialiased">
 				<ThemeProvider
 					attribute="class"
-					defaultTheme="dark"
+					defaultTheme="system"
 					enableSystem
 					disableTransitionOnChange
 				>
